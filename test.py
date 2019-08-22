1bdcdb43cc9e5d2fa960a8fb9340f860109d169c
godelscat <shimrac@outlook.com>
2019-08-22 17:51:27 +0200

    update

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 import torch
 from model import YOLO, load_weights
 from decode import decode
+from loss import yolo_loss
 import numpy as np
 
 # set random seed
@@ -11,13 +12,19 @@ anchors = np.array([
     [10, 13], [16, 30], [33, 23], [30, 61], [62, 45], 
     [59, 119], [116, 90], [156, 198], [373, 326]
 ], dtype=np.float)
-
 anchors = anchors / 416
-
 anchor_mask = np.array([[6, 7, 8], [3, 4, 5], [0, 1, 2]])
+image_size = (416, 416)
 
 # input
 x = torch.rand(1, 3, 416, 416)
+
+tars = np.random.randint(0, 80, size=(1, 5, 5))
+tars = tars.astype(float)
+for j in range(4):
+    tars[..., j] = tars[...,j] / 80 
+
+#y = torch.from_numpy(tars)
 
 weightsfile = './weights/yolov3.weights'
 
@@ -43,3 +50,6 @@ print(osx[0,:20])
 """
 
 # check loss
+
+loss = yolo_loss(out, tars, anchors, image_size)
+print(loss)
